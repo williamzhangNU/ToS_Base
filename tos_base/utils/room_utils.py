4e94d392b599ed1f0569dead547e7c21c8154fa0
williamzhangNU <pingyuezhang2029@u.northwestern.edu>
2025-07-19 14:31:41 -0500

    update

--- tos_base/utils/room_utils.py
+++ tos_base/utils/room_utils.py
@@ -159,41 +159,6 @@ def generate_room_for_rotation_eval(
             
     return objects
 
-
-
-# for initializing visual-based
-def initialize_room_from_json(json_data: Dict[str, Any]) -> Room:
-    """
-    Initialize a Room from your metadata JSON, which now has:
-      - objects: list of {oid, model, pos:{x,y,z}, rot:{x,y,z}, size:[w,h]}
-      - cameras: list of {id, label, position:{x,y,z}, rotation:{y}}
-      - room_size, screen_size, etc.
-    """
-    # Rotation to orientation vector mapping
-    rotation_map = {0: np.array([0, 1]), 90: np.array([1, 0]), 180: np.array([0, -1]), 270: np.array([-1, 0])}
-    
-    # 1) Parse all objects
-    objects = []
-    for obj in json_data['objects']:
-        objects.append(Object(
-            name=obj['model'],
-            pos=np.array([obj["pos"]["x"], obj["pos"]["z"]]),
-            ori=rotation_map.get(obj["rot"]["y"]),
-            has_orientation=True
-        ))
-    # 2) Room size metadata
-    room_name = json_data.get("name", "room_from_json")
-    #room_size = tuple(json_data.get("room_size", []))  # if your Room supports it
-
-    # 3) Build and return
-    agent_pos = [camera['position'] for camera in json_data['cameras'] if camera['id'] == 'central'][0]
-    agent_pos = np.array([agent_pos["x"], agent_pos["z"]])
-    for obj in objects:
-        obj.pos -= agent_pos
-    return Room(objects=objects, name=room_name, agent=Agent())
-
-
-
 if __name__ == '__main__':
     room = generate_room_for_rotation_eval(
         n=10,
