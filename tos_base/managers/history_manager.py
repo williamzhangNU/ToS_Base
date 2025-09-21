51cf496964b64ca48e4323616f3a35cfc5018450
tenacious <tenacioustommy6@gmail.com>
2025-09-21 16:38:12 +0800

    add think directory

--- tos_base/managers/history_manager.py
+++ tos_base/managers/history_manager.py
@@ -25,8 +25,8 @@ class HistoryManager:
     Example: gpt-4o/1d54fa/vision/active/
     """
 
-    def __init__(self, observation_config:Dict, model_config:Dict ,room_dict: Dict, agent_dict: Dict, output_dir:str,
-                 exp_override: bool = False, eval_override: bool = False, cogmap_override: bool = False, all_override: bool = False, task_type: str = None):
+    def __init__(self, observation_config: Dict, model_config: Dict , room_dict: Dict, agent_dict: Dict, output_dir:str,
+                 eval_override: bool = False, all_override: bool = False, task_type: str = None):
         # only explore turn logs are saved
         self.exploration_turn_logs: List[Dict] = []
         self.evaluation_turn_logs: Dict[str, Dict[str, Dict]] = {}
@@ -36,9 +36,9 @@ class HistoryManager:
             self.model_path,
             self._generate_room_key(room_dict, agent_dict),
             observation_config['render_mode'],
-            observation_config['exp_type']
+            observation_config['exp_type'],
+            "think" if observation_config['prompt_config']["enable_think"] else "nothink",
         ))
-        model_config_path = os.path.join(self.model_path, "model_config.json")
         if observation_config['exp_type'] == 'passive':
             self.output_dir = os.path.join(self.output_dir, observation_config["proxy_agent"])
         self.exploration_path = os.path.join(self.output_dir, EXPLORATION_LOG_BASENAME)
@@ -50,9 +50,6 @@ class HistoryManager:
         if all_override:
             if os.path.exists(self.output_dir):
                 shutil.rmtree(self.output_dir)
-        elif exp_override and observation_config['exp_type'] == 'active':
-            if os.path.exists(self.output_dir):
-                shutil.rmtree(self.output_dir)
 
         self._load()
         if eval_override:
@@ -60,9 +57,6 @@ class HistoryManager:
                 self.evaluation_turn_logs[task_type] = {}
         os.makedirs(self.output_dir, exist_ok=True)
         os.makedirs(os.path.join(self.output_dir, IMAGES_DIRNAME), exist_ok=True)
-        if not os.path.exists(model_config_path):
-            with open(model_config_path, "w") as f:
-                json.dump(model_config, f, ensure_ascii=False, indent=2)
 
         # Save per-sample config/meta for convenience
         self._write_config(observation_config, model_config, room_dict, agent_dict)
